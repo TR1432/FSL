f026d5c339d7b4be2b7dbb2b7f38d251754c7010
TR1432 <117490906+TR1432@users.noreply.github.com>
2024-10-18 18:29:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1187,4 +1187,39 @@ def reset_points():
     else:
         return redirect("/")
 
+
+def load_swep_teams(csv_file):
+    with open(csv_file, newline='') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            team = SwepLeagueTeam(
+                team_name=row[0]
+            )
+            db.session.add(team)
+        db.session.commit()
+
+def load_players(csv_file):
+    script_dir = os.path.dirname(__file__)
+    file_path = os.path.join(script_dir, csv_file)
+    with open(file_path, newline='') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            team = SwepLeagueTeam.query.filter_by(team_name=row[2]).first()
+            if team:
+                player = Player(
+                    name=row[0],
+                    position=row[1],
+                    price=row[3],
+                    SwepLeagueTeam_id=team.id
+                )
+                db.session.add(player)
+        db.session.commit()   
+        
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    load_swep_teams('Swepleageteams.csv')
+    load_players('players.csv')
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
